952b92aed86b59dcba16b0f70248bd5eee1bd416
philippe <phil.maia@gmail.com>
2018-03-12 13:28:00 -0300

    Add xlsxwriter support

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
 import io
 from flask import Flask, render_template, request, send_file
-from api_modules.module import request_router, request_xlsx
-import pandas as pd
-from pandas.io.json import json_normalize
+from api_modules.module import request_router, json_to_excel
+import xlsxwriter
+import json
+
 
 app = Flask(__name__)
 
@@ -141,28 +142,28 @@ def get_team_new_work():
     return request_router(request.full_path)
 
 
-@app.route('/report_percent_readme')
-def get_report_percent_readme():
-    query_result = request_xlsx(request.full_path)
-    query_result = json_normalize(query_result)
-    query_result["readme"] = query_result["readme"].astype(str)
+@app.route('/report_consolidate_readme')
+def get_report_consolidate_readme():
+    query_result = request_router(request.full_path)
     output = io.BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    query_result.to_excel(writer, sheet_name='Sheet1', index=False)
-    writer.close()
+    data = json.loads(query_result)
+    wb = xlsxwriter.Workbook(output)
+    ws = wb.add_worksheet()
+    json_to_excel(ws, data)
+    wb.close()
     output.seek(0)
-    return send_file(output, attachment_filename="{}".format("report_percent_readme.xlsx"), as_attachment=True)
+    return send_file(output, attachment_filename="{}".format("report_consolidate_readme.xlsx"), as_attachment=True)
 
 
 @app.route('/report_readme')
 def get_report_readme():
-    query_result = request_xlsx(request.full_path)
-    query_result = json_normalize(query_result)
-    query_result["repository"] = query_result["repository"].astype(str)
+    query_result = request_router(request.full_path)
     output = io.BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    query_result.to_excel(writer, sheet_name='Sheet1', index=False)
-    writer.close()
+    data = json.loads(query_result)
+    wb = xlsxwriter.Workbook(output)
+    ws = wb.add_worksheet()
+    json_to_excel(ws, data)
+    wb.close()
     output.seek(0)
     return send_file(output, attachment_filename="{}".format("report_readme.xlsx"), as_attachment=True)
 
--- api_modules/module.py
+++ api_modules/module.py
@@ -6,11 +6,18 @@ import os
 def request_router(path):
     router_url = os.getenv("ROUTER_URL")
     r = requests.get(router_url + path).json()
-    print(r)
     return json.dumps(r)
 
 
-def request_xlsx(path):
-    router_url = os.getenv("ROUTER_URL")
-    r = requests.get(router_url + path).json()
-    return r
+def json_to_excel(ws, data, row=0):
+    for item in data:
+        col = 0
+        for key, value in item.items():
+            if row == 0:
+                ws.write(row, col, key)
+            else:
+                if value is None:
+                    value = 'None'
+                ws.write(row, col, value)
+            col += 1
+        row += 1
